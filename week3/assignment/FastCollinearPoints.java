58e09dfcd5ee2c4b23c9aefb3ec2e7e4baaa813c
Jeff Cheng <jeff32191182@gmail.com>
2020-03-14 21:47:02 -0700

    modify.

--- week3/assignment/FastCollinearPoints.java
+++ week3/assignment/FastCollinearPoints.java
@@ -1,71 +1,70 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FastCollinearPoints {
+	private final List<LineSegment> lineSegments = new ArrayList<LineSegment>();
 
-    private final ArrayList<LineSegment> lineSegments = new ArrayList<>();
+	public FastCollinearPoints(Point[] points) {
+		if (points == null)
+			throw new IllegalArgumentException();
+		
+		for (Point p : points)
+			if (p == null)
+				throw new IllegalArgumentException();
 
-    public FastCollinearPoints(Point[] points) {
-        validate(points);
-        int numOfPoints = points.length;
-        if (numOfPoints < 4) { return; }
-        Point[] pointSet = points.clone();
-        Arrays.sort(pointSet);
-        // Given a point p, the following method determines
-        // whether p participates in a set of 4 or more collinear points.
-        for (int i = 0; i < numOfPoints; i++) {
-            // Think of p as the origin.
-            // Sort the points according to the slopes they makes with p.
-            Point[] ps = pointSet.clone();
-            Arrays.sort(ps, ps[i].slopeOrder());
-            // For each other point q, determine the slope it makes with p.
-            // Also note that ps[0] == pointSet[i]
-            final Point anchor = ps[0];
-            double slopeSlow = anchor.slopeTo(ps[1]), slopeFast;
-            for (int idxSlow = 1, idxFast; idxSlow < numOfPoints - 2; idxSlow = idxFast, slopeSlow = slopeFast) {
-                idxFast = idxSlow + 1;
-                do {
-                    slopeFast = anchor.slopeTo(ps[idxFast++]);
-                } while (slopeSlow == slopeFast && idxFast < numOfPoints);
-                idxFast--;
-                // Check if any 3 or more adjacent points in the sorted order
-                // have equal slopes with respect to p.
-                // If so, these points, together with p, are collinear.
-                int numOfAdjacentPoint = idxFast - idxSlow;
-                if (numOfAdjacentPoint >= 3) {
-                    // sort the array as previous sort is unstable
-                    Point[] segment = new Point[numOfAdjacentPoint + 1];
-                    segment[0] = anchor;
-                    System.arraycopy(ps, idxSlow, segment, 1, numOfAdjacentPoint);
-                    Arrays.sort(segment);
-                    // make sure no duplicate subsegment
-                    if (segment[0] == anchor) {
-                        lineSegments.add(new LineSegment(anchor, segment[numOfAdjacentPoint]));
-                    }
-                }
-            }
-        }
-    }
+		for (int i = 0; i < points.length - 1; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+				if ((points[i].compareTo(points[j])) == 0)
+					throw new IllegalArgumentException();
+			}
+		}
+		int pointsNums = points.length;
+		if (pointsNums < 4)
+			return;
 
-    public int numberOfSegments() {
-        return lineSegments.size();
-    }
+		Point[] pointSet = points.clone();
+		Arrays.sort(pointSet);
 
-    public LineSegment[] segments() {
-        return lineSegments.toArray(new LineSegment[numberOfSegments()]);
-    }
+		for (int i = 0; i < pointsNums; i++) {
 
-    private void validate(Point[] points) {
-        if (points == null) { throw new IllegalArgumentException(); }
+			Point[] ps = pointSet.clone();
+			Arrays.sort(ps, ps[i].slopeOrder());
 
-        for (Point point : points) {
-            if (point == null) { throw new IllegalArgumentException(); }
-        }
+			final Point anchor = ps[0];
+			double slopeSlow = anchor.slopeTo(ps[1]);
+			double slopeFast;
+			int idxSlow = 1;
+			int idxFast =0;
+			while (idxSlow < pointsNums - 2) {
+				idxFast = idxSlow + 1;
+				do {
+					slopeFast = anchor.slopeTo(ps[idxFast++]);
+				} while (slopeSlow == slopeFast && idxFast < pointsNums);
+				int numOfAdjacentPoint = idxFast - idxSlow;
+				if (numOfAdjacentPoint >= 3) {
+					// sort the array as previous sort is unstable
+					Point[] segment = new Point[numOfAdjacentPoint + 1];
+					segment[0] = anchor;
+					System.arraycopy(ps, idxSlow, segment, 1, numOfAdjacentPoint);
+					Arrays.sort(segment);
+					// make sure no duplicate subsegment
+					if (segment[0] == anchor) {
+						lineSegments.add(new LineSegment(anchor, segment[numOfAdjacentPoint]));
+					}
+				}
+				slopeSlow = slopeFast;
+				idxSlow = idxFast;
+			}
+		}
 
-        for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                if ((points[j].equals(points[i]))) { throw new IllegalArgumentException(); }
-            }
-        }
-    }
+	} // finds all line segments containing 4 or more points
+
+	public int numberOfSegments() {
+		return lineSegments.size();
+	} // the number of line segments
+
+	public LineSegment[] segments() {
+		return lineSegments.toArray(new LineSegment[numberOfSegments()]);
+	} // the line segments
 }
